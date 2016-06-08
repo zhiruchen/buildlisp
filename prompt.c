26e199a5b35d49fc0968081fbe1e8658a35b5571
zhiruchen <zhiruchen1992@foxmail.com>
2016-06-08 08:56:43 +0800

    修改

--- prompt.c
+++ prompt.c
@@ -3,6 +3,7 @@
 
 #include<editline/readline.h>
 
+//https://www-s.acm.illinois.edu/webmonkeys/book/c_guide/
 
 int main(int argc, char const *argv[]) {
   /* code */
@@ -13,11 +14,17 @@ int main(int argc, char const *argv[]) {
     /* code */
 
     /* output our prompt and get the input*/
-    char* input = readline("Lispy> ");
+    char* input = readline("lispy--> ");
     /* Add input to history*/
     add_history(input);
 
-    printf("No, you are a %s\n", input);
+    printf("So, what you just typed is %s\n", input);
+    printf("整数 %d\n", 100);
+    printf("字符 %c\n", 'x');
+    printf("浮点数 %.2f\n", 1.005);
+    printf("十六进制 %x\n", 100);
+    printf("八进制 %o\n", 100);
+    printf("%e\n", 1000.006);
     /* free retrived input*/
     free(input);
   }
